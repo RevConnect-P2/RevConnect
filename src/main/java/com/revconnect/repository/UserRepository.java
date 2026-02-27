8fa919c8a8356a0e0f7c41a0e23db50ebc6942c0
Allam Sandeep <sandeep30139@gmail.com>
2026-02-28 01:06:36 +0530

    Updated authentication module

--- src/main/java/com/revconnect/repository/UserRepository.java
+++ src/main/java/com/revconnect/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package com.revconnect.repository;
 
-public interface UserRepository {
+import com.revconnect.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository
+        extends JpaRepository<User, Long>
+{
+
+
+    // check email exists
+    Optional<User> findByEmail(String email);
+
+
+
+    // ✅ ADD THIS (IMPORTANT)
+    // check username exists
+    Optional<User> findByUsername(String username);
+
+
+
 }
