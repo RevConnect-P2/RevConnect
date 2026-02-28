ee18c8ff96c9a72de7ad41037787b83b50157cee
dhanush shetty <sdhanush7348932205@gmail.com>
2026-02-28 14:33:26 +0530

    Completed post management module with ownership authorization with swagger and postman testing

--- src/main/java/com/revconnect/controller/PostController.java
+++ src/main/java/com/revconnect/controller/PostController.java
@@ -3,12 +3,17 @@ package com.revconnect.controller;
 import com.revconnect.dto.request.PostCreateRequest;
 import com.revconnect.dto.response.PostResponse;
 import com.revconnect.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Posts", description = "Post management APIs")
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
@@ -19,6 +24,12 @@ public class PostController {
     /**
      * Create a new post
      */
+    @Operation(summary = "Create a new post")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Post created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @PostMapping
     public ResponseEntity<PostResponse> createPost(
             @RequestParam Long userId,
@@ -31,6 +42,12 @@ public class PostController {
     /**
      * Update existing post (only owner)
      */
+    @Operation(summary = "Update post by postId")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Post created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @PutMapping("/{postId}")
     public ResponseEntity<PostResponse> updatePost(
             @PathVariable Long postId,
@@ -44,6 +61,12 @@ public class PostController {
     /**
      * Delete post (only owner)
      */
+    @Operation(summary = "Delete a post by postId")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Post created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @DeleteMapping("/{postId}")
     public ResponseEntity<String> deletePost(
             @PathVariable Long postId,
@@ -56,6 +79,12 @@ public class PostController {
     /**
      * Get posts created by logged-in user
      */
+    @Operation(summary = "Get a posts by user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Post created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @GetMapping("/my")
     public ResponseEntity<List<PostResponse>> getMyPosts(
             @RequestParam Long userId
@@ -67,6 +96,12 @@ public class PostController {
     /**
      * Get post by ID
      */
+    @Operation(summary = "Get a post by Id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Post created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @GetMapping("/{postId}")
     public ResponseEntity<PostResponse> getPostById(
             @PathVariable Long postId
