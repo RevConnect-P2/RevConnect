4ea472bb7501281d7e9c97f420eccfbbf44b72d6
Allam Sandeep <sandeep30139@gmail.com>
2026-03-08 13:24:15 +0530

    Added search by user

--- src/main/java/com/revconnect/controller/UserController.java
+++ src/main/java/com/revconnect/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.revconnect.controller;
 
+import com.revconnect.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // 🔍 SEARCH USERNAMES API
+    @GetMapping("/search")
+    public List<String> searchUsers(@RequestParam String keyword) {
+
+        return userService.searchUsernames(keyword);
+
+    }
 }
--- src/main/java/com/revconnect/entity/Post.java
+++ src/main/java/com/revconnect/entity/Post.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "POST_ID")
     private Long postId;
 
