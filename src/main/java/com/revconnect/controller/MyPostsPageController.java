28e95fbb41aedda4017654c38524c77ea6e02fc4
Allam Sandeep <sandeep30139@gmail.com>
2026-03-10 12:03:27 +0530

    updated my post and saved post controller

--- src/main/java/com/revconnect/controller/MyPostsPageController.java
+++ src/main/java/com/revconnect/controller/MyPostsPageController.java
@@ -1,6 +1,9 @@
 package com.revconnect.controller;
 
 import com.revconnect.entity.User;
+import com.revconnect.service.ConnectionService;
+import com.revconnect.service.FollowService;
+import com.revconnect.service.PostService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyPostsPageController {
+
+    private final FollowService followService;
+    private final ConnectionService connectionService;
+    private final PostService postService;
+
+    public MyPostsPageController(FollowService followService, ConnectionService connectionService, PostService postService) {
+        this.followService = followService;
+        this.connectionService = connectionService;
+        this.postService = postService;
+    }
 
     @GetMapping("/posts")
     public String myPostsPage(HttpSession session, Model model) {
@@ -19,11 +32,23 @@ public class MyPostsPageController {
 
         model.addAttribute("user", user);
 
-        // temporary stats
-        model.addAttribute("connectionsCount", 0);
-        model.addAttribute("followersCount", 0);
-        model.addAttribute("followingCount", 0);
+        long followers =
+                followService.getFollowersCount(user.getUserId());
 
+        long following =
+                followService.getFollowingCount(user.getUserId());
+
+        long connections =
+                connectionService.getConnectionsCount(user.getUserId());
+
+        model.addAttribute("followersCount", followers);
+        model.addAttribute("followingCount", following);
+        model.addAttribute("connectionsCount", connections);
+
+        model.addAttribute(
+                "trendingTags",
+                postService.getTrendingHashtags()
+        );
         return "posts/my-posts";
     }
 }
